a80742c2025a31b1ad88ba0ed4e450f512553db9
scrin123 <scrinnn95@gmail.com>
2019-01-20 16:30:51 +0700

    Начало проекта по расчету крафта в бдо

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -14,61 +14,18 @@ use app\models\User;
 use app\models\Howmuch_owen;
 use app\models\Howmuch_owenForm;
 
-class MainController extends Controller{
+class MainController extends Controller
+{
+protected $user;
+    public $layout = 'main';
 
-                public $layout = 'main';
+    public function actionIndex()
+    {
 
-                public function actionIndex(){
-                    
-                    return $this->render('index');
-                }
-                public function actionCalculate(){
-
-                    $calculate_model= new CalculateForm();
-                    $model_user = new User();
-                    if($calculate_model->load(Yii::$app->request->post())){
-                        // Преобразование даты в формат ГГ-Месяц-ДЕНЬ,нужно для нормальной записи в базу данных
-                       // $date = Yii::$app->request->post()['CalculateForm']['date'];
-                       // $pieces = explode(".", $date);
-                       // $date=$pieces[2]."-".$pieces[1]."-".$pieces[0];
-//----------------------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------------------------------
-                       //var_dump(User::find()->where(['name' => Yii::$app->request->post()['CalculateForm']['name']])->asArray()->all());
-//----------------------Проверка на  существование фирмы в базе данных--------------------------------------------------------------------------
-                        if ((User::find()->where(['name' => Yii::$app->request->post()['CalculateForm']['name']])->asArray()->all()) == NULL) {
-                            echo "Добавлено";
-                            $model_user->name=Yii::$app->request->post()['CalculateForm']['name'];
-                            $model_user->save();
-                        }else{
-                            echo "Такая фирма существует";
-                            
-                        }
-                    }
-//----------------------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------------------------------
-                     return $this->render('calculate',compact('calculate_model'));
-                }
-
-        public function actionSumma(){
-                    $Howmuch_owen_model= new Howmuch_owenForm();
-                    $model_Howmuch_owen= new Howmuch_owen();
-                    $model_user = new User();
-                    if($Howmuch_owen_model->load(Yii::$app->request->post())){
-                            echo Yii::$app->request->post('Howmuch_owenForm')['summa'];
-
-                            return $this->render('summa',compact('Howmuch_owen_model'));
-                            
-                        
-
- 
-        }
-
-
+        return $this->render('index');
+    }
 
 }
-
-
-
 
 
 ?>
